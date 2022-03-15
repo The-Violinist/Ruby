aca419fa8c96a34aee1fc53ad43b450402cef117
The-Violinist <violinist.david@yahoo.com>
2022-03-15 16:58:04 -0700

    First Ruby

---
+++ authenticator.rb
@@ -0,0 +1,52 @@
+# Project #1 from "The Complete Ruby on Rails Developer Course from Udemy, taught by Mashrur Hossain"
+# This program puts together for and while loops and uses conditionals to check input against an array of hashes
+
+# Array containing username and password combos as hashes
+profiles = [
+    {username: "David", password: "password1" },
+    {username: "John", password: "password2" },
+    {username: "Bill", password: "password3" },
+    {username: "Mark", password: "password4" }
+]
+
+# Take input from a user in the form of a username and password
+def get_input
+    puts "Please enter a username: "
+    user_name = gets.chomp
+    puts "Please enter a password: "
+    pass_word = gets.chomp
+    return user_name, pass_word                                                         # Return the combo as an array
+end
+
+# Loop through all hashes in 'profiles' and check for matching combos with the input
+def look_at_profiles(profiles, creds)
+    for profile in profiles do
+        if profile[:username] == creds[0] && profile[:password] == creds[1]
+            return profile                                                              # Return the hash entry if it is a match
+        else
+            x = false
+        end
+    end
+    return x                                                                            # Return 'false' if there is no match
+end
+
+# 
+def check_input(profiles)
+    i = 0                                                                               # Counter for the number of attempts
+    while true do
+        creds = get_input                                                               # Retrieve the input from the user
+        reply = look_at_profiles(profiles, creds)                                       # Check the input against the 'profiles' array
+        if reply == false                                                               # If there is not a match, increment the counter
+            i += 1
+        else
+            puts reply                                                                  # If there is a match print the hash
+            i = 0
+        end
+        if i == 3                                                                       # If the counter reaches 3, end the loop
+            puts "You have exceeded the number of attempts."
+            break
+        end
+    end
+end
+
+check_input(profiles)
---
+++ basic_commands.rb
@@ -0,0 +1,54 @@
+# puts "Enter a number"                                                     # Get user input
+# num = gets.chomp
+# puts "The number you entered is #{num}"
+# puts num.to_i.class                                                       # Convert to an integer
+# puts num.to_s.class                                                       # Convert to a string
+# (40).times { print "-"}
+
+
+# puts "\nEnter a data input"
+# data = gets.chomp
+
+# if data.class == String                                                     # Conditional
+#     puts "That is data type string"
+# elsif data.class == Integer
+#     puts "That is an integer"
+# else
+#     puts "That is a different data type"
+# end
+# (40).times { print "-"}
+
+
+# arr = [1,2,3,4,5]                                                           # Create an array
+# puts
+# puts "#{arr.reverse}\n"                                                     # Reverse
+# puts "#{arr.shuffle}\n"                                                     # Randomize order
+# puts "#{arr.append(6)}\n"                                                   # Add item at end. Use unshift to add at the beginning
+# x = arr.pop                                                                 # Remove the last item and assign it to a variable
+# puts "The popped number is #{x}"
+# puts "The array after popping #{arr}"                                       # Use uniq to remove duplicate values
+# (40).times { print "-"}
+
+
+# y = (1..9).to_a
+# puts "\nArray created from range: #{y}"
+# puts "Iterating through the items in the array"
+# y.each { |item| puts (item.to_i * 2)}                                       # Loop thru and print each item multiplied by 2
+# w = y.join(" ")                                                             # Concatenates array items with a space between characters
+# puts "#{w}"
+# puts "#{w.split(" ")}"
+
+myHash = {a: 1, b: 2, c: 3, d: "Hello"}                                        # Crete dictionary (hash)
+puts myHash[:c]                                                                # Print value by key
+(40).times { print "-"}
+puts
+myHash.each { |key, value| puts value}                                         # Iterate thru and print values
+(40).times { print "-"}
+puts
+myHash.delete(:d)
+myHash.select { |k, v| puts v.odd? }
+(40).times { print "-"}
+puts
+myHash.each { |k, v| myHash.delete(k) if v > 2 }                               # Remove an item with conditional
+puts myHash
+
